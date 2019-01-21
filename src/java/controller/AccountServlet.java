724e3e57a5f065d836dc9f5decd5f6b372138392
Danfeng Wang <danfeng.wang0027@stud.hkr.se>
2019-01-21 16:14:51 +0100

    correct second commit

---
+++ src/java/controller/AccountServlet.java
@@ -0,0 +1,27 @@
+
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccountServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/mode/HibernateUtil.java
+++ src/java/mode/HibernateUtil.java
@@ -1,4 +1,4 @@
-package controller;
+package mode;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
