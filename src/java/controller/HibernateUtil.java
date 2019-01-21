4a66529422de32d915b9b59826dc4c7f9d8421cc
Danfeng Wang <danfeng.wang0027@stud.hkr.se>
2019-01-21 15:50:02 +0100

    Fourth Commit - upload Deployment descriptor file web.xml.

---
+++ src/java/controller/HibernateUtil.java
@@ -0,0 +1,32 @@
+package controller;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration config = new Configuration();
+            ServiceRegistry sr = new StandardServiceRegistryBuilder()
+                    .applySettings(config.getProperties()).build();
+            sessionFactory = config.configure().buildSessionFactory(sr);
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static Session openSession() {
+        return sessionFactory.openSession();
+    }
+}
